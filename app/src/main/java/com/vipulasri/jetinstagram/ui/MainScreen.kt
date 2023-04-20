9f3a9bc115367ed377ff23d9df069cd1cfc48481
Reshma2497 <reshmashaik2497@gmail.com>
2023-04-20 09:20:58 +0100

    Added Post and Favourite pages

--- app/src/main/java/com/vipulasri/jetinstagram/ui/MainScreen.kt
+++ app/src/main/java/com/vipulasri/jetinstagram/ui/MainScreen.kt
@@ -30,7 +30,9 @@ import com.vipulasri.jetinstagram.ui.HomeSection.Profile
 import com.vipulasri.jetinstagram.ui.HomeSection.Reels
 import com.vipulasri.jetinstagram.ui.components.bottomBarHeight
 import com.vipulasri.jetinstagram.ui.components.icon
+import com.vipulasri.jetinstagram.ui.favourite.Favorite
 import com.vipulasri.jetinstagram.ui.home.Home
+import com.vipulasri.jetinstagram.ui.newpost.NewPost
 import com.vipulasri.jetinstagram.ui.reels.Reels
 
 @ExperimentalFoundationApi
@@ -40,126 +42,128 @@ fun MainScreen() {
     val sectionState = remember { mutableStateOf(Home) }
 
     val navItems = HomeSection.values()
-      .toList()
-  Scaffold(
-      bottomBar = {
-        BottomBar(
-            items = navItems,
-            currentSection = sectionState.value,
-            onSectionSelected = { sectionState.value = it},
+        .toList()
+    Scaffold(
+        bottomBar = {
+            BottomBar(
+                items = navItems,
+                currentSection = sectionState.value,
+                onSectionSelected = { sectionState.value = it },
+            )
+        }) { innerPadding ->
+        val modifier = Modifier.padding(innerPadding)
+        Crossfade(
+            modifier = modifier,
+            targetState = sectionState.value
         )
-      }) { innerPadding ->
-    val modifier = Modifier.padding(innerPadding)
-    Crossfade(
-        modifier = modifier,
-        targetState = sectionState.value)
-    { section ->
-        when (section) {
-            Home -> Home()
-            Reels -> Reels()
-            Add -> Content(title = "Add Post options")
-            Favorite -> Content(title = "Favorite")
-            Profile -> Content(title = "Profile")
+        { section ->
+            when (section) {
+                Home -> Home()
+                Reels -> Reels()
+                Add -> NewPost()
+                Favorite -> Favorite()
+                Profile -> Content(title = "Profile")
+            }
         }
     }
-  }
 }
 
 @Composable
 private fun Content(title: String) {
-  Box(
-      modifier = Modifier.fillMaxSize(),
-      contentAlignment = Alignment.Center
-  ) {
-    Text(
-        text = title,
-        textAlign = TextAlign.Center,
-        style = MaterialTheme.typography.h5
-    )
-  }
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = title,
+            textAlign = TextAlign.Center,
+            style = MaterialTheme.typography.h5
+        )
+    }
 }
 
 @Composable
 private fun BottomBar(
-  items: List<HomeSection>,
-  currentSection: HomeSection,
-  onSectionSelected: (HomeSection) -> Unit,
+    items: List<HomeSection>,
+    currentSection: HomeSection,
+    onSectionSelected: (HomeSection) -> Unit,
 ) {
-  BottomNavigation(
-      modifier = Modifier.height(bottomBarHeight),
-      backgroundColor = MaterialTheme.colors.background,
-      contentColor = contentColorFor(MaterialTheme.colors.background)
-  ) {
-    items.forEach { section ->
+    BottomNavigation(
+        modifier = Modifier.height(bottomBarHeight),
+        backgroundColor = MaterialTheme.colors.background,
+        contentColor = contentColorFor(MaterialTheme.colors.background)
+    ) {
+        items.forEach { section ->
 
-      val selected = section == currentSection
+            val selected = section == currentSection
 
-      val iconRes = if (selected) section.selectedIcon else section.icon
+            val iconRes = if (selected) section.selectedIcon else section.icon
 
-      BottomNavigationItem(
-          icon = {
+            BottomNavigationItem(
+                icon = {
 
-            if (section == Profile) {
-              BottomBarProfile(selected)
-            } else {
-              Icon(
-                  ImageBitmap.imageResource(id = iconRes),
-                  modifier = Modifier.icon(),
-                  contentDescription = ""
-              )
-            }
+                    if (section == Profile) {
+                        BottomBarProfile(selected)
+                    } else {
+                        Icon(
+                            ImageBitmap.imageResource(id = iconRes),
+                            modifier = Modifier.icon(),
+                            contentDescription = ""
+                        )
+                    }
 
-          },
-          selected = selected,
-          onClick = { onSectionSelected(section) },
-          alwaysShowLabel = false
-      )
+                },
+                selected = selected,
+                onClick = { onSectionSelected(section) },
+                alwaysShowLabel = false
+            )
+        }
     }
-  }
 }
 
 @Composable
 private fun BottomBarProfile(isSelected: Boolean) {
-  val shape = CircleShape
+    val shape = CircleShape
 
-  val borderModifier = if (isSelected) {
-    Modifier
-        .border(
-            color = Color.LightGray,
-            width = 1.dp,
-            shape = shape
-        )
-  } else Modifier
+    val borderModifier = if (isSelected) {
+        Modifier
+            .border(
+                color = Color.LightGray,
+                width = 1.dp,
+                shape = shape
+            )
+    } else Modifier
 
-  val padding = if (isSelected) 3.dp else 0.dp
+    val padding = if (isSelected) 3.dp else 0.dp
 
-  Box(
-      modifier = borderModifier
-  ) {
     Box(
-        modifier = Modifier.icon()
-            .padding(padding)
-            .background(color = Color.LightGray, shape = shape)
-            .clip(shape)
+        modifier = borderModifier
     ) {
-        Image(
-            painter = rememberImagePainter(currentUser.image),
-            contentDescription = null,
-            modifier = Modifier.fillMaxSize()
-        )
+        Box(
+            modifier = Modifier
+                .icon()
+                .padding(padding)
+                .background(color = Color.LightGray, shape = shape)
+                .clip(shape)
+        ) {
+            Image(
+                painter = rememberImagePainter(currentUser.image),
+                contentDescription = null,
+                modifier = Modifier.fillMaxSize()
+            )
+        }
     }
-  }
 
 }
 
 private enum class HomeSection(
-  val icon: Int,
-  val selectedIcon: Int
+    val icon: Int,
+    val selectedIcon: Int
 ) {
-  Home(R.drawable.ic_outlined_home, R.drawable.ic_filled_home),
-  Reels(R.drawable.ic_outlined_reels, R.drawable.ic_filled_reels),
-  Add(R.drawable.ic_outlined_add, R.drawable.ic_outlined_add),
-  Favorite(R.drawable.ic_outlined_favorite, R.drawable.ic_filled_favorite),
-  Profile(R.drawable.ic_outlined_reels, R.drawable.ic_outlined_reels)
+    Home(R.drawable.ic_outlined_home, R.drawable.ic_filled_home),
+    Reels(R.drawable.ic_outlined_reels, R.drawable.ic_filled_reels),
+    Add(R.drawable.ic_outlined_add, R.drawable.ic_outlined_add),
+    Favorite(R.drawable.ic_outlined_favorite, R.drawable.ic_filled_favorite),
+    Profile(R.drawable.ic_outlined_reels, R.drawable.ic_outlined_reels)
 }
 
